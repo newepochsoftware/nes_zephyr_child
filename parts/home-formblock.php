baa695de2f221960325a2ae6940202fbb0fda2a0
Tom Madrid <tom@newepochsoftware.com>
2018-07-05 14:35:53 -0700

    TLS Cert seal

--- parts/home-formblock.php
+++ parts/home-formblock.php
@@ -19,6 +19,7 @@
     <div class="grid-x">
       <div class="cell small-12 medium-6 text-right">
         <p>Lead Verified saves a history of every attempted lead event or transaction, provides heatmaps of visitor interaction, serves as a failover solution for lead posts, scores every lead using our proprietary grading system, and issues a certificate of authenticity for each lead. Extensible to any event, transaction, or visitor interaction, Lead Verified can verify and record engagement, and identify abuse and fraud.</p>
+        <table width="135" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose Symantec SSL for secure e-commerce and confidential communications."><tr><td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.websecurity.norton.com/getseal?host_name=newepochsoftware.com&amp;size=M&amp;use_flash=NO&amp;use_transparent=Yes&amp;lang=en"></script><br /><a href="https://www.websecurity.symantec.com/ssl-certificate" target="_blank"  style="color:#000000; text-decoration:none; font:bold 10px verdana,sans-serif; letter-spacing:.5px;text-align:center; margin:0px; padding:0px;"> ABOUT SSL CERTIFICATES</a></td></tr></table>
       </div>
       <div class="cell small-12 medium-6">
         <?php echo do_shortcode( '[gravityform id="3" title="false" description="false"]' ); ?>
