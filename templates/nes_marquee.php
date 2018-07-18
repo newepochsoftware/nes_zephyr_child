f38cbfca901887ec8f461ef9c22d5ad259b830e0
Tom Madrid <tom@newepochsoftware.com>
2018-07-18 16:19:52 -0700

    Z index fixes

--- templates/nes_marquee.php
+++ templates/nes_marquee.php
@@ -16,24 +16,21 @@ $themePath = get_stylesheet_directory_uri();
   </div>
 
   <div class="nes_menu">
+
     <div class="grid-container">
-      
       <div class="platform-menu" id="platform-menu">
         <?php get_template_part( 'parts/home', 'menu' ); ?>
       </div>
-
-      <div class="grid-x logo-grid">
-        <div class="cell small-6">
+      <div class="grid-x logo-grid navbar" id="navbar">
+        <div class="cell small-6 large-6">
           <div class="logo"><a href="/" class="homelink"></a></div>
         </div>
-        <div class="cell small-6 menublock">
-
+        <div class="cell small-6 large-6 menublock">
           <button id="toggle-platform-menu" class="hamburger hamburger--boring" type="button">
             <span class="hamburger-box">
               <span class="hamburger-inner"></span>
             </span>
           </button>
-
         </div>
       </div>
     </div>
