d9905a966c4656abcaabf57e5c2fc3bf221a10a3
Tom Madrid <tom@newepochsoftware.com>
2018-08-07 15:19:50 -0700

    First round of updates
    - Removed animation effect on features section
    - Screencaps are more visible on tablet/landscape phone

--- parts/home-features.php
+++ parts/home-features.php
@@ -11,21 +11,21 @@ $themePath = get_stylesheet_directory_uri();
   <div class="grid-container">
     
     <div class="grid-x feature-row">
-      <div class="cell small-12 medium-8 text-right">
+      <div class="cell small-12 medium-6 large-8 text-right">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Lead Intelligence</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M270,0C120.88,0,0,120.88,0,270S120.88,540,270,540,540,419.12,540,270,419.12,0,270,0ZM235.38,212.78V390.86a29.45,29.45,0,0,1-29.44,29.39H182.55a27.23,27.23,0,1,1,0-15.56h23.39a13.66,13.66,0,0,0,13.85-13.83V212.78a7.76,7.76,0,0,1,7.8-7.78C232.35,205,235.81,208.46,235.38,212.78ZM270.45,466.5H270a27.32,27.32,0,0,1-26.41-28.09,28,28,0,0,1,19.48-25.51V186.41a7.8,7.8,0,0,1,15.59,0V412.9a23.38,23.38,0,0,1,11.69,7.78,28.63,28.63,0,0,1,7.36,19.45A27.54,27.54,0,0,1,270.45,466.5Zm114.32-26.8a27.4,27.4,0,0,1-26-19.45H335.4A29.45,29.45,0,0,1,306,390.86V212.78a7.8,7.8,0,0,1,15.59,0V390.86a13.66,13.66,0,0,0,13.85,13.83h23.39a27.2,27.2,0,1,1,26,35Zm57.58-115.4a105.44,105.44,0,0,1-69.28,34.14H330.21V331.21h42a74.19,74.19,0,0,0,50.23-25.07,79.23,79.23,0,0,0,20.78-53.59c0-35.45-24.25-67.43-57.59-76.51a13.48,13.48,0,0,1-10-10.8,91.75,91.75,0,0,0-140.29-61.81,93.45,93.45,0,0,0-29.88,31.12A13.07,13.07,0,0,1,191.21,141a43.87,43.87,0,0,0-52.39,43.22,76.16,76.16,0,0,0,.87,9.51,14.16,14.16,0,0,1-5.2,13.4,65.92,65.92,0,0,0-27.28,53.6A72.47,72.47,0,0,0,125.4,308.3c11.69,13.84,27.28,22.05,43.73,22.91h42.44v27.23H167.83c-23.81-1.29-46.33-12.53-63.22-31.55C88.59,308.3,79.5,285,79.5,261.19a93.78,93.78,0,0,1,32-70.45c0-2.16-.43-4.33-.43-6.05a71.57,71.57,0,0,1,75.34-71.32,121.88,121.88,0,0,1,33.78-32.85,117.29,117.29,0,0,1,64.51-19,120.37,120.37,0,0,1,80.54,31.12,117.73,117.73,0,0,1,35.51,59.65c40.7,14.69,69.28,55.32,69.71,100.28A104.63,104.63,0,0,1,442.35,324.3Z"/><ellipse class="cls-1" cx="156.57" cy="412.47" rx="11.69" ry="11.67"/><path class="cls-1" d="M271.32,427.6h-.43a11.45,11.45,0,0,0-11.26,11.24,9.84,9.84,0,0,0,3,8.21,11.09,11.09,0,0,0,8.23,3.46,11.45,11.45,0,0,0,11.25-11.24,9.8,9.8,0,0,0-3-8.21A10.84,10.84,0,0,0,271.32,427.6Z"/><path class="cls-1" d="M384.77,400.8a11.67,11.67,0,1,0,11.69,11.67A11.64,11.64,0,0,0,384.77,400.8Z"/></g></g></svg>
         <h3>LEAD INTELLIGENCE</h3>
         <p>Lead Verified records every interaction with your web site and provides heatmaps of visitor interactions so you can see which portions of your web site are getting engagement, and which portions may be overlooked. Use heatmaps for conversion rate optimization (CRO) and to ensure that disclaimers required for compliance are seen by your site visitors.</p>
       </div>
-      <div class="cell small-12 medium-4 max5" data-aos="fade-up">
+      <div class="cell small-12 medium-6 large-4 max5">
         <div class="tablet straight heatmap"></div>
       </div>
     </div>
 
     <div class="grid-x feature-row">
-      <div class="cell small-12 small-order-2 medium-4 medium-order-1" data-aos="fade-up">
+      <div class="cell small-12 small-order-2 medium-6 large-4 medium-order-1">
         <div class="tablet straight compliance left"></div>
       </div>
-      <div class="cell small-12 small-order-1 medium-8 medium-order-2">
+      <div class="cell small-12 small-order-1 medium-6 large-8 medium-order-2">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Regulatory Compliance</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M377.57,175.87l-58.35-58.34a11,11,0,0,0-5.14-2.92V181h66.41A11,11,0,0,0,377.57,175.87Z"/><path class="cls-1" d="M270,0C120.88,0,0,120.88,0,270S120.88,540,270,540,540,419.12,540,270,419.12,0,270,0ZM403.08,414.68a33.39,33.39,0,0,1-33.37,33.38H169.38A33.39,33.39,0,0,1,136,414.68V125.39a33.5,33.5,0,0,1,33.38-33.45h142a33.4,33.4,0,0,1,23.58,9.81l58.34,58.34h0a33.55,33.55,0,0,1,9.8,23.64V232L380.83,254.2v-51H308.51a16.61,16.61,0,0,1-16.69-16.65V114.2H169.38a11.22,11.22,0,0,0-11.13,11.19V414.68a11.16,11.16,0,0,0,11.13,11.13H369.71a11.16,11.16,0,0,0,11.12-11.13v-92.8l22.25-22.25ZM186.08,181H264a5.56,5.56,0,0,1,5.56,5.57v11.12a5.56,5.56,0,0,1-5.56,5.57h-77.9a5.57,5.57,0,0,1-5.57-5.57V186.54A5.57,5.57,0,0,1,186.08,181Zm-5.57-27.82V142a5.56,5.56,0,0,1,5.57-5.56H264a5.56,5.56,0,0,1,5.56,5.56v11.13a5.56,5.56,0,0,1-5.56,5.56h-77.9A5.56,5.56,0,0,1,180.51,153.15ZM202.77,359h8.63a7.73,7.73,0,0,0,7.37-5.32l12.67-38a16.71,16.71,0,0,1,31.69,0l9.65,28.95c13.74-11.25,37.58-6.74,45.9,9.85a7.81,7.81,0,0,0,7.07,4.52h10.56a11.13,11.13,0,1,1,0,22.26H325.74a29.91,29.91,0,0,1-26.86-16.58,7.71,7.71,0,0,0-7.09-4.55,7.58,7.58,0,0,0-7,4.31l-5.33,10.66a11.14,11.14,0,0,1-9.95,6.17c-.26,0-.52,0-.78,0a11.1,11.1,0,0,1-9.78-7.57l-11.69-35.11-7.39,22.18a30,30,0,0,1-28.5,20.54h-8.63a11.13,11.13,0,1,1,0-22.26Zm249-119.84-88.94,88.95a7.74,7.74,0,0,1-10.94,0l-50.28-50.27a7.74,7.74,0,0,1,0-10.94L312.51,256a7.74,7.74,0,0,1,10.94,0l33.87,33.87,72.54-72.54a7.74,7.74,0,0,1,10.94,0l10.93,10.94A7.71,7.71,0,0,1,451.73,239.19Z"/></g></g></svg>
         <h3>REGULATORY COMPLIANCE</h3>
         <p>As a Google Cloud-based application, Lead Verified offers unprecedented protection and compliance infrastructure to support TCPA, GDPR, and MiFID requirements. Archive leads for TCPA protection and as a backup to primary systems. Save leads with a data retention window ranging from 6 months to 4+ years. Store lead screen captures and videos for GDPR and MiFID compliance. Maintain proof of submission in the event of legal proceedings.</p>
@@ -33,21 +33,21 @@ $themePath = get_stylesheet_directory_uri();
     </div>
 
     <div class="grid-x feature-row">
-      <div class="cell small-12 medium-8 text-right">
+      <div class="cell small-12 medium-6 large-8 text-right">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#666;}</style></defs><title>Lead Scoring</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><polygon class="cls-1" points="209.52 276.67 299.26 276.67 254.39 168.33 209.52 276.67"/><path class="cls-1" d="M270,0C120.88,0,0,120.88,0,270S120.88,540,270,540,540,419.12,540,270,419.12,0,270,0Zm65.27,137.55h34.82V103.33h26v34.22h35V162.1h-35v34.21h-26V162.1H335.27Zm-5.7,283.06H300.49V449H278.92V420.61H250V400.22h28.92V371.8h21.57v28.42h29.08ZM344.94,387l-25.06-60.64H188.5L163.44,387H96.33l126.13-283h64.68L413.67,387Zm93.64-128.48H418v20.14H402.67V258.55h-20.5V244.09h20.5V224H418v20.14h20.62Z"/></g></g></svg>
         <h3>LEAD SCORING</h3>
         <p>Lead Verified serves as a failover solution for lead posts, scores every lead using our proprietary grading system, and issues a certificate of authenticity for each lead. Use Lead Verified to ensure data integrity before leads enter your system and are routed to sales reps, partners, or lead buyers. Selectively post only the leads that meet your criteria.</p>
       </div>
-      <div class="cell small-12 medium-4 max5" data-aos="fade-up">
+      <div class="cell small-12 medium-6 large-4 max5">
         <div class="tablet straight score"></div>
       </div>
     </div>
 
     <div class="grid-x feature-row">
-      <div class="cell small-12 small-order-2 medium-4 medium-order-1" data-aos="fade-up">
+      <div class="cell small-12 small-order-2 medium-6 large-4 medium-order-1">
         <div class="tablet straight capture left"></div>
       </div>
-      <div class="cell small-12 small-order-1 medium-8 medium-order-2">
+      <div class="cell small-12 small-order-1 medium-6 large-8 medium-order-2">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Proof of Submission</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M403.09,203.88A78.9,78.9,0,1,0,482,282.77,78.88,78.88,0,0,0,403.09,203.88Zm55.15,59L393.1,326.29a5.91,5.91,0,0,1-8.38,0L350.1,291.58a4,4,0,0,1,0-5.57l7-6.95a4,4,0,0,1,5.57,0L389,305.39l56.8-55.08a3.91,3.91,0,0,1,5.57,0l6.95,7A3.92,3.92,0,0,1,458.24,262.88Z"/><path class="cls-1" d="M471.87,371.09a6.1,6.1,0,0,1-1.72-4.19v-6.51a102.54,102.54,0,1,1,10.55-10.55h6.51a5.85,5.85,0,0,1,4.19,1.72l26.21,26.21A269,269,0,0,0,540,270C540,120.88,419.12,0,270,0S0,120.88,0,270,120.88,540,270,540A269.9,269.9,0,0,0,504.57,403.78ZM220.6,69.26a79.51,79.51,0,1,1-79.51,79.51A79.51,79.51,0,0,1,220.6,69.26Zm5.14,404.18c-69.77-6.89-151.6-87-144.28-141.8-.43-45.32,37.39-83.48,83.48-83.48h10.38a108.2,108.2,0,0,0,90.56,0h10.37c1.62,0,14.11.43,15.66.49C264,312,310,401,400.41,402.69,400.41,402.69,343.76,485.5,225.74,473.44Z"/></g></g></svg>
         <h3>PROOF OF SUBMISSION</h3>
         <p>Lead Verified creates a snapshot and video of every form fill or specified event and saves each lead with a unique token to the Google Drive or FTP location of your choice. Along with the video, our software zips the site as seen by the visitor along with user-generated content, location, IP, hardware, browser, and software details.</p>
@@ -55,21 +55,21 @@ $themePath = get_stylesheet_directory_uri();
     </div>
 
     <div class="grid-x feature-row">
-      <div class="cell small-12 medium-8 text-right">
+      <div class="cell small-12 medium-6 large-8 text-right">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Data Warehousing</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M158.4,203.06c-23.82,7.56-38.67,18.23-38.67,30v20.73c0,22.83,55.7,41.45,124.36,41.45s124.36-18.62,124.36-41.45V235.39c-25.31,5.47-52.4,7.41-72.54,7.41C254.06,242.8,181.83,234.48,158.4,203.06Z"/><path class="cls-1" d="M119.73,350.64v27.53c0,22.83,55.7,41.45,124.36,41.45S368.45,401,368.45,378.17V350.64c-26.71,18.79-75.62,27.53-124.36,27.53S146.45,369.43,119.73,350.64Z"/><path class="cls-1" d="M119.73,288.46V316c0,22.83,55.7,41.45,124.36,41.45S368.45,338.82,368.45,316V288.46C341.74,307.24,292.83,316,244.09,316S146.45,307.24,119.73,288.46Z"/><path class="cls-1" d="M270,0C120.88,0,0,120.88,0,270S120.88,540,270,540,540,419.12,540,270,419.12,0,270,0ZM441,180.61c0,24.66-23,40.12-52.34,49.46a38.47,38.47,0,0,1,.52,6.25v142.5c0,50-94.47,62.18-145.09,62.18C193.63,441,99,428.91,99,378.82V236.32c0-24.66,23-40.12,52.34-49.45a38.69,38.69,0,0,1-.52-6.26V161.18c0-50,94.47-62.18,145.09-62.18C346.37,99,441,111.09,441,161.18Z"/><path class="cls-1" d="M295.91,116.49c-68.66,0-124.36,18.62-124.36,41.45v20.73c0,22.83,55.7,41.45,124.36,41.45s124.36-18.62,124.36-41.45V157.94C420.27,135.11,364.57,116.49,295.91,116.49Z"/></g></g></svg>
         <h3>DATA WAREHOUSING</h3>
         <p>Archive lead data in user-specified FTP (on-premise) or Google Drive locations. Activate as a data warehousing or data pass-through application. Use Lead Verified to back-up primary systems and provide a failover solution with full recovery capability.</p>
       </div>
-      <div class="cell small-12 medium-4 max5" data-aos="fade-up">
+      <div class="cell small-12 medium-6 large-4 max5">
         <div class="tablet straight data"></div>
       </div>
     </div>
 
     <div class="grid-x feature-row">
-      <div class="cell small-12 small-order-2 medium-4 medium-order-1" data-aos="fade-up">
+      <div class="cell small-12 small-order-2 medium-6 large-4 medium-order-1">
         <div class="tablet straight reports left"></div>
       </div>
-      <div class="cell small-12 small-order-1 medium-8 medium-order-2">
+      <div class="cell small-12 small-order-1 medium-6 large-8 medium-order-2">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 540"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Fraud Prevention</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M270,0C120.88,0,0,120.88,0,270S120.88,540,270,540,540,419.12,540,270,419.12,0,270,0ZM220.6,69.26a79.51,79.51,0,1,1-79.51,79.51A79.51,79.51,0,0,1,220.6,69.26Zm5.14,404.18c-69.77-6.89-151.6-87-144.28-141.8-.43-45.32,37.39-83.48,83.48-83.48h10.38a108.2,108.2,0,0,0,90.56,0h10.37c1.62,0,3.11.43,4.66.49a36.35,36.35,0,0,0-.68,4.48c0,59.19,28.08,105,61.62,131.68,15.69,14.55,37.53,22.24,47.56,24.88C389.41,409.69,343.76,485.5,225.74,473.44ZM396.9,386.19a18.91,18.91,0,0,1-6.9,1.31,18.68,18.68,0,0,1-6.9-1.31c-39.9-15.6-82.6-63.58-82.6-132.94a16.82,16.82,0,0,1,11-15.48l71.6-28a18.91,18.91,0,0,1,6.9-1.31,18.68,18.68,0,0,1,6.9,1.31l71.6,28a16.82,16.82,0,0,1,11,15.48C479.5,330.63,428.78,373.76,396.9,386.19Z"/><path class="cls-1" d="M400.57,363.57c24.17-13.92,58.73-48.35,59.66-107.27L400.57,233Z"/></g></g></svg>
         <h3>FRAUD PREVENTION</h3>
         <p>Lead Verified gives your customers – internal and external – the peace of mind they need to utilize real-time leads by the thousand. Extensible to any event, transaction, or visitor interaction, Lead Verified can verify and record engagement, and help prevent abuse and fraud.</p>
